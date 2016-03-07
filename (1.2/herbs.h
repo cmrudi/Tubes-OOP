dab0486a492baad6f40ef7d4e1acc7ce1b7797df
fazarafi <faza.thirafi@gmail.com>
2016-03-07 10:56:47 +0700

    Added files via upload

---
+++ (1.2/herbs.h
@@ -0,0 +1,16 @@
+#ifndef HERBS_H_
+#define HERBS_H_
+
+#include "tumbuhan.h"
+
+class Herbs : public tumbuhan {
+	//This is Abstract Class
+	//The 3rd level of abstract class
+	public:
+		//Method
+		void muncul();
+		void regenerasi_shield();
+		
+};
+
+#endif
